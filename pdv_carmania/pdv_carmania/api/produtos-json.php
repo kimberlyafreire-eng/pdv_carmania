a8f92ad3fdb97ad21cb6077bb427b24bfc3c51af
kimberlyafreire-eng <kimberly.afreire@gmail.com>
2025-10-14 11:55:34 -0300

    Adjust product image handling and card layout

--- pdv_carmania/pdv_carmania/api/produtos-json.php
+++ pdv_carmania/pdv_carmania/api/produtos-json.php
@@ -11,6 +11,55 @@ if (!file_exists($dbFile)) {
     exit;
 }
 
+$baseImagemDir = realpath(__DIR__ . '/../imagens') ?: __DIR__ . '/../imagens';
+$baseImagemUrl = '/pdv_carmania/imagens';
+
+if (!function_exists('resolverImagemProduto')) {
+    function resolverImagemProduto(array $row, string $baseDir, string $baseUrl): string
+    {
+        $idProduto = $row['id'];
+        $imagemLocal = $row['imagem_local'];
+
+        $baseDirNormalizado = rtrim(str_replace('\\', '/', $baseDir), '/');
+        $baseUrlNormalizado = rtrim($baseUrl, '/');
+
+        $normalizarCaminho = static function (string $path) use ($baseDirNormalizado, $baseUrlNormalizado): string {
+            $caminhoNormalizado = str_replace('\\', '/', $path);
+            if (strpos($caminhoNormalizado, $baseDirNormalizado . '/') === 0) {
+                $relativo = substr($caminhoNormalizado, strlen($baseDirNormalizado) + 1) ?: '';
+            } else {
+                $relativo = ltrim($caminhoNormalizado, '/');
+            }
+
+            return $baseUrlNormalizado . '/' . ltrim($relativo, '/');
+        };
+
+        if (!empty($imagemLocal)) {
+            $caminho = rtrim($baseDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $imagemLocal;
+            if (file_exists($caminho)) {
+                return $normalizarCaminho($caminho);
+            }
+        }
+
+        $pastaProduto = rtrim($baseDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $idProduto;
+        if (is_dir($pastaProduto)) {
+            $padroes = ['*.jpg', '*.jpeg', '*.png', '*.webp', '*.gif'];
+            $arquivos = [];
+            foreach ($padroes as $padrao) {
+                $encontrados = glob($pastaProduto . DIRECTORY_SEPARATOR . $padrao, GLOB_NOSORT) ?: [];
+                $arquivos = array_merge($arquivos, $encontrados);
+            }
+
+            if (!empty($arquivos)) {
+                sort($arquivos);
+                return $normalizarCaminho($arquivos[0]);
+            }
+        }
+
+        return $baseUrlNormalizado . '/sem-imagem.png';
+    }
+}
+
 try {
     $db = new SQLite3($dbFile);
 
@@ -23,9 +72,7 @@ try {
             'codigo' => $row['codigo'],
             'nome' => $row['nome'],
             'preco' => (float) $row['preco'],
-            'imagemURL' => $row['imagem_local']
-                ? "/pdv_carmania/imagens/" . $row['imagem_local']
-                : "/pdv_carmania/imagens/sem-imagem.png"
+            'imagemURL' => resolverImagemProduto($row, $baseImagemDir, $baseImagemUrl)
         ];
     }
 
